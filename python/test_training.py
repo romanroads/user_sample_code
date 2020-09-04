617b09c75d7e1a9075f1b9c0d31d928069070d5e
Joe Xing <joe.xing.ai@gmail.com>
2020-09-03 17:57:03 -0700

    restart signal

--- python/test_training.py
+++ python/test_training.py
@@ -19,6 +19,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
 import json
 
 try:
@@ -31,6 +32,7 @@ LOG_PORT = 5555
 CONTROL_PORT = 5556
 LOG_IP_PORT = "tcp://%s:%s" % (LOG_IP, LOG_PORT)
 CONTROL_IP_PORT = "tcp://%s:%s" % (LOG_IP, CONTROL_PORT)
+TIME_TO_RESTART = 10
 
 
 def test_using_nanomsg_socket():
@@ -49,10 +51,27 @@ def test_using_nanomsg_socket():
             payload = packet[header_length:]
             payload = payload.decode('unicode_escape')
             dictionary = json.loads(payload)
-            timestamp = dictionary["Timestamp"]
+            timestamp = int(dictionary["Timestamp"])
+
+            if "initial_time" not in locals():
+                initial_time = timestamp
+
             print("[INFO] timestamp %s" % timestamp)
-            socket_control.send(('%s:KeepLane:1.0' % timestamp).encode('unicode_escape'))
+            decision_command = {
+                "Timestamp": timestamp,
+                "Behavior": "KeepLane",
+                "Duration": 1.0,
+                "Acceleration": 1.2
+            }
+
+            if (timestamp - initial_time) / 1000. > TIME_TO_RESTART:
+                decision_command["Restart"] = True
+                initial_time = timestamp
+
+            msg = json.dumps(decision_command).encode('unicode_escape')
+            socket_control.send(msg)
     except:
+        print("[ERROR] caught exception: ", sys.exc_info()[0])
         pass
 
 
